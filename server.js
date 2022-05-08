5368be88e4f3c4223b908b69df667cc4251fa370
Indrasen Singh <insin@microsoft.com>
2022-05-09 00:14:38 +0530

    Added compression, expires nd cache header

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var compression = require('compression')
 
 const app = express();
+
+// compress all responses
+app.use(compression())
+
+app.use(function (req, res, next) {
+  res.set('Cache-Control', 'public, max-age=345600'); // 4 days
+  res.set("Expires", new Date(Date.now() + 2592000000 * 30).toUTCString()); // 30 months
+  next();
+})
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/wordle-solver-app'));
@@ -13,4 +23,4 @@ res.sendFile(path.join(__dirname+'/dist/wordle-solver-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 4202);
